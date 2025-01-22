bfeb05571c5407d82c7ae722b2fa8d181dd6064a
bocharsky-bw <bocharsky.bw@gmail.com>
2025-01-22 12:13:21 +0100

    Implement increment/decrement of product quantity in JS

---
+++ assets/controllers/product_controller.js
@@ -0,0 +1,18 @@
+import { Controller } from '@hotwired/stimulus';
+
+export default class extends Controller {
+    static targets = ['quantityInput']
+
+    connect() {
+    }
+
+    incrementQuantity() {
+        const value = parseInt(this.quantityInputTarget.value);
+        this.quantityInputTarget.value = value > 0 ? value + 1 : 1;
+    }
+
+    decrementQuantity() {
+        const value = parseInt(this.quantityInputTarget.value);
+        this.quantityInputTarget.value = value > 1 ? value - 1 : 1;
+    }
+}
